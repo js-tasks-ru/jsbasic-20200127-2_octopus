2b9a1604936774245776b684f7cb7ebb5c1c90d6
octopus <dshl.srp@gmail.com>
2020-02-01 17:03:45 +0300

    1-module 3-task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -4,4 +4,10 @@
  * @returns {string}
  */
 function ucFirst(str) {
+  let NewStr = ' ';
+  if (str.length < 2) {
+    return NewStr = `${str.charAt(str.length - 1).toUpperCase()}`;
+  }
+  NewStr = `${str[0].toUpperCase() + str.slice(1)}`;
+  return NewStr;
 }
