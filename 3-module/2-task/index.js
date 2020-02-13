60c8b4e51cd9fb3c8dc3ed631fee67e639d588e6
octopus <dshl.srp@gmail.com>
2020-02-13 15:45:30 +0300

    3-module 2-task

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,19 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  let newArr = str.split(' ');
+  newArr = newArr.join().split(',');
+  console.log(newArr.join().split(','));
+  let minMaxArr = [];
+  for (let element of newArr) {
+    if (!isNaN(+element)) {
+      minMaxArr.push(+element);
+    }
+  }
+  console.log(minMaxArr);
+  let result = {
+    max: Math.max(...minMaxArr),
+    min: Math.min(...minMaxArr),
+  };
+  return result;
 }
